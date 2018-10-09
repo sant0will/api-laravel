f44b639a9100e676a2066dc06df688a7826f8c55
Willan Alves dos Santos <30411014+sant0will@users.noreply.github.com>
2018-10-09 20:53:59 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +13,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('api/', 'Api');
-Route::get('api/store_user', 'Api@store_user');
+Route::get('api/store_user', , function () {
+    echo User::all()->toJson();
+});
+Route::get('api/store_user', , function () {
+    $user = new User();
+    $user->first_name = $_GET["first_name"];
+    $user->last_name = $_GET["last_name"];
+    $user->phone = $_GET["phone"];
+    $user->email = $_GET["email"];
+    if($user->save()){
+        echo 1;
+    }else{
+        echo 0;
+    }
+});
