df2468afac32b584f0801b8df80036919f0593d3
Willan Alves dos Santos <30411014+sant0will@users.noreply.github.com>
2018-10-09 20:58:09 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('api/', function () {
-    dd("chegou");
+    dd(User::all()->toJson());
     echo User::all()->toJson();
 });
 Route::get('api/store_user', function () {
