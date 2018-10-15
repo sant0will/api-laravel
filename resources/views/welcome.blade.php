937cf7ce8308ffa376da2d3328bd4efe855de0e3
Willan Alves dos Santos <30411014+sant0will@users.noreply.github.com>
2018-10-15 19:33:46 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Cadastro</title>
-
+        <link rel="icon" href="{!! asset('one.png') !!}" sizes="16x16"/>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
